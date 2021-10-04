502d501414312c022f57b40a832073c5c664cea3
Daniel Fonseca da Silva <developer-web@programmer.net>
2021-10-04 16:36:35 -0300

    Adicionada a validação básica user para email

--- colchonet/app/models/user.rb
+++ colchonet/app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
     validates_presence_of :email, :full_name, :location, :password
     validates_confirmation_of :password
     validates_length_of :bio, :minimum => 30, :maximum => 200, :allow_blank => false
+    validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+    validates_uniqueness_of :email
 
 end
