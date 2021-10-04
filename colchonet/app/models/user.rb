943a3fb6f5b047ec260d203a559c438497650b8e
Daniel Fonseca da Silva <developer-web@programmer.net>
2021-10-04 15:04:11 -0300

    Adicionada a validação user para tamanho da bio

--- colchonet/app/models/user.rb
+++ colchonet/app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
 
     validates_presence_of :email, :full_name, :location, :password
     validates_confirmation_of :password
+    validates_length_of :bio, :minimum => 30, :maximum => 200, :allow_blank => false
 
 end
