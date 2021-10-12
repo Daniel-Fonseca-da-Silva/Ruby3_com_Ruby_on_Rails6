df464b663791c2293d084f42b539c6a6b871ec00
Daniel Fonseca da Silva <developer-web@programmer.net>
2021-10-12 18:31:07 -0300

    Criada tabela restaurante com rake db:migrate

--- vota_prato/db/migrate/20211012203437_create_restaurantes.rb
+++ vota_prato/db/migrate/20211012203437_create_restaurantes.rb
@@ -1,7 +1,8 @@
 class CreateRestaurantes < ActiveRecord::Migration[6.1]
   def change
     create_table :restaurantes do |t|
-
+      t.string :nome, limit: 80
+      t.string :endereco
       t.timestamps
     end
   end
