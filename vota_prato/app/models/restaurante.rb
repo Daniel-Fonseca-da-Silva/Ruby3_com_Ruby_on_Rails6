1f44dd5a4c504ba3128662875395c988650aefb7
Daniel Fonseca da Silva <developer-web@programmer.net>
2021-10-13 02:06:02 -0300

    Efetuadas validações na entidade restaurante

--- vota_prato/app/models/restaurante.rb
+++ vota_prato/app/models/restaurante.rb
@@ -1,2 +1,17 @@
 class Restaurante < ApplicationRecord
+
+  validates_presence_of :nome, message: "campo obrigatório"
+  validates_presence_of :endereco, message: "campo obrigatório"
+  validates_presence_of :especialidade, message: "campo obrigatório"
+
+  validates_uniqueness_of :nome, message: "nome já cadastrado"
+  validates_uniqueness_of :endereco, message: "Endereço já cadastrado"
+
+  validate :primeira_letra_deve_ser_maiscula
+
+  private
+  def primeira_letra_deve_ser_maiscula
+    errors.add(:nome, "primeira letra deve ser maiúscula") unless nome = ~/[A-Z].*/
+  end
+
 end
